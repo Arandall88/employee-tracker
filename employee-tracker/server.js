5f5ed5635cd44db712e6c56c96332c209190d558
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-07 20:19:14 -0500

    added server.js , package.json, inquirer prompt

---
+++ employee-tracker/server.js
@@ -0,0 +1,31 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoletable = require("console-table");
+
+//  CONNECTION TO SQL DATABASE
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_trackerdb"
+})
+
+// function to add departments , roles, employees
+
+//var inquirer = require('inquirer');
+inquirer
+.prompt([
+  /* Pass your questions in here */
+  "Please pick a choice."
+
+])
+.then(answers => {
+  // Use user feedback for... whatever!!
+})
+.catch(error => {
+  if(error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else when wrong
+  }
